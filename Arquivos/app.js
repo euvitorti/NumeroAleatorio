7df10fffc07249c7fff6531fe74990c3c2a4e5a3
Vítor Santos <jvitor.ti03@gmail.com>
2024-01-17 20:40:35 -0300

    Update app.js

---
+++ Arquivos/app.js
@@ -0,0 +1,10 @@
+// MANIPULANDO O DOM
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hello World!';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolhe um número entre 1 e 10';
+
+function verificarChute(){
+    
+}
