94a9654a8a24f1a8f197cf5bd5414cd72d16240a
Vítor Santos <jvitor.ti03@gmail.com>
2024-01-18 09:04:13 -0300

    Update app.js

--- Arquivos/app.js
+++ Arquivos/app.js
@@ -4,14 +4,59 @@ function exiberTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exiberTextoNaTela('h1', 'Hello World!');
-exiberTextoNaTela('p', 'Escolha um número entre 1 e 100');
-
-function verificarChute() {
-    let chute = document.querySelector('input').value;
+function exiberMensagemInicial() {
+    exiberTextoNaTela('h1', 'Hello World!');
+    exiberTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
+exiberMensagemInicial();
+
+let tentativas = 1;
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Parabéns! Acertou com ${tentativas} ${palavraTentativa}`;
+
+        exiberTextoNaTela('h1', 'Acertou');
+        exiberTextoNaTela('p', mensagemTentativas);
+
+        // DESABILITAR O ATRIBUTO NO HTML
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        exiberTextoNaTela('h1', 'Quaseee...');
+        if (chute > numeroSecreto) {
+            exiberTextoNaTela('p', 'Um pouco menos');
+        } else {
+            exiberTextoNaTela('p', 'Um pouco mais');
+        }
+        tentativas++;
+
+        limparCampo();
+    }
+}
+
+// GERAR NÚMERO ALEATÓRIO
 let numeroSecreto = gerarNumero();
 function gerarNumero() {
-    return parseInt(Math.random() * 100 + 1);
+    return parseInt(Math.random() * 5 + 1);
+}
+
+
+//FAZER COM ARROWFUNCTION
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    exiberMensagemInicial();
+    numeroSecreto = gerarNumero();
+    limparCampo();
+    tentativas = 1;
+
+    //DESABILITANDO O BOTÃO
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
