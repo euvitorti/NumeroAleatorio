49e61cb67f881aadcc70ade8d94ebed2d6bae083
Vítor Santos <jvitor.ti03@gmail.com>
2024-01-19 19:28:30 -0300

    Modificadno o h1

--- Arquivos/app.js
+++ Arquivos/app.js
@@ -11,7 +11,7 @@ function exiberTextoNaTela(tag, texto) {
 }
 
 function exiberMensagemInicial() {
-    exiberTextoNaTela('h1', 'Hello World!');
+    exiberTextoNaTela('h1', 'Tente a sorte');
     exiberTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
